e15dd4135943060ae00171f81aeb466c708374be
tlanders <tlanders@landersconsulting.biz>
2024-01-26 22:13:21 -0600

    created basic prompt with user and system messages

---
+++ src/main/java/biz/lci/springaidemos/PromptController.java
@@ -0,0 +1,33 @@
+package biz.lci.springaidemos;
+
+import org.springframework.ai.chat.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class PromptController {
+    @Autowired
+    protected ChatClient chatClient;
+
+    @GetMapping("/prompt")
+    public Map<String,String> chat(
+            @RequestParam(defaultValue = "Tell me an important fact") String userPrompt,
+            @RequestParam(defaultValue = "You are a pirate and only like to talk about pirate stuff") String systemPrompt) {
+        UserMessage userMessage = new UserMessage(userPrompt);
+        SystemMessage systemMessage = new SystemMessage(systemPrompt);
+        Prompt prompt = new Prompt(List.of(userMessage, systemMessage));
+        return Map.of(
+                "userPrompt", userPrompt,
+                "systemPrompt", systemPrompt,
+                "response", chatClient.call(userPrompt)
+        );
+    }
+}
